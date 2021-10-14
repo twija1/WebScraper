85b60a8da3d1274a77e748d511954c6d8247b8bc
twija <238354@student.pwr.edu.pl>
2021-10-14 15:13:50 +0200

    feature: multiple checks

--- index.js
+++ index.js
@@ -5,19 +5,33 @@ const express = require('express')
 
 const app = express()
 
+const urls = [
+    'https://www.mibosport.com/en/hudy-alu-nut-m4-black-10pcs',
+    'https://www.mibosport.com/en/muchmore-fleta-zx-bearing-set-fr?bbResetZone=1&bbSetCurrency=EUR'
+]
 
-const url = 'https://www.mibosport.com/en/hudy-alu-nut-m4-black-10pcs'
-// const url = 'https://www.mibosport.com/en/muchmore-fleta-zx-bearing-set-fr?bbResetZone=1&bbSetCurrency=EUR'
+const classes = '.add-to-cart-form .state'
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
+const desiredText = 'in stock'
 
-        $('.add-to-cart-form .state', html).each(function (){
-            const tit = $(this).text()
-            console.log(tit)
-        })
-    }).catch(err => console.log(err))
+const scrape = function (url, classes, desiredText) {
+
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $(classes, html).each(function () {
+                const tit = $(this).text()
+                if (tit.includes(desiredText))
+                    console.log('available', url)
+                else
+                    console.log('not available', url)
+            })
+        }).catch(err => console.log(err))
+}
+
+urls.forEach(url => scrape(url, classes, desiredText))
+
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
